45aa696f8b2c7f85c75e27ba0d71a1010ea91c45
Gary McNeish <garymcneish@gmail.com>
2021-01-09 21:22:14 +0000

    Update urls.py

--- app/hello_django/urls.py
+++ app/hello_django/urls.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 
 
 def home(request):
-    return JsonResponse({"hello": "gary555!!!!!!!!!!!!!!!!"})
+    return JsonResponse({"hello": "finaltest"})
 
 
 urlpatterns = [
